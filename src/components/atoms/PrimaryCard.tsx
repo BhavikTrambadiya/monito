0d0c9eb1f717d0dfe7163d3b0b46b66eb9a8ab6f
Bhavik Trambadiya <bhaviktrambadiya@gmail.com>
2023-06-14 23:03:58 +0530

    Revert "first version of web-version design"
    This reverts commit 75ba9a3f86ff70a0e299ae471596153ef226302e.

--- src/components/atoms/PrimaryCard.tsx
+++
@@ -1,9 +0,0 @@
-import {PropsWithChildren} from "react";
-
-export default function PrimaryCard({className = '', children, ...props}: PropsWithChildren<{ className?: string }>) {
-    return (
-        <div className={"bg-white shadow-main rounded-xl p-2 " + className}  {...props}>
-            {children}
-        </div>
-    )
-}
